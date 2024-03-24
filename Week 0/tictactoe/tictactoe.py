30dbea29a2175859e327d1302e100df2d3d51fea
CRowland4 <cjrowland4@gmail.com>
2024-03-24 16:32:06 -0500

    Implement utility function of tictactoe problem

--- Week 0/tictactoe/tictactoe.py
+++ Week 0/tictactoe/tictactoe.py
@@ -92,7 +92,7 @@ def winner(board: list[list]) -> None | str:
     return None
 
 
-def terminal(board: list[list]):
+def terminal(board: list[list]) -> bool:
     """
     Returns True if game is over, False otherwise.
     """
@@ -106,11 +106,17 @@ def terminal(board: list[list]):
     return False
 
 
-def utility(board):
+def utility(board: list[list]) -> int:
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
+    winning_player = winner(board)
+    if winning_player == X:
+        return 1
+    if winning_player == O:
+        return -1
+
+    return 0
 
 
 def minimax(board):
